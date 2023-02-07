4f7f389484698c94c6ef0737a69ba119c4c84c61
DelphineM7 <95476951+DelphineM7@users.noreply.github.com>
2023-02-07 19:00:11 +0100

    ajouter des fleurs

--- swamp.js
+++ swamp.js
@@ -4,7 +4,9 @@ let wiw = window.innerWidth;
 let wih = window.innerHeight;
 let MaScène = new THREE.Scene();
 let Macamera = new THREE.PerspectiveCamera(75, wiw/wih, 0.1, 1000) // j'ai mis au piff
-let moteurRendu = new THREE.WebGLRenderer({antialias : true});
-moteurRendu.setSize(wiw,wih);       
-document.body.appendChild(moteurRendu.domElement)
+//let moteurRendu = new THREE.WebGLRenderer({antialias : true});
+//moteurRendu.setSize(wiw,wih);       
+//document.body.appendChild(moteurRendu.domElement)
+//let couleurSelectionnee='#4CC3D9';
+
 
